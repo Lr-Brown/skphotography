48e7a2b9ea765f43e8d41f41ee0e798ff9c5b2dc
Logan Brown <lrbrown826@gmail.com>
2021-11-30 22:29:56 -0500

    fix regex

--- src/views/contact-view/index.js
+++ src/views/contact-view/index.js
@@ -13,8 +13,7 @@ const ContactView = () => {
 
   const submit = () => {
     const isValidEmail = (email) => {
-      const regex =
-        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
       return regex.test(String(email).toLowerCase())
     }
 
